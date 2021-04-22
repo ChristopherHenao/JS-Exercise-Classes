b59078013f6821f15ffec16c133ae2532822becd
ChristoherHenao <cq.henao@gmail.com>
2021-04-22 17:35:11 -0400

    task 4 solved

--- index.js
+++ index.js
@@ -122,7 +122,8 @@ class Airplane {
      this.age = attr.age,
      this.location = attr.location
    }
-    speak(){
+    //Class Methods 
+   speak(){
       return `Hello my name is ${this.name}, I am from ${this.location}`; 
     }
   }
@@ -141,8 +142,20 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+   constructor(attr){
+     super(attr);
+     this.specialty = attr.specialty,
+     this.favLanguage = attr.favLanguage,
+     this.catchPhrase = attr.catchPhrase
+   }
+    demo(subject){
+      return `Today we are learning about ${subject}`; 
+    }
 
+    grade(studentObj, subject){
+      return `${studentObj.name} receives a perfect score on ${subject}`;
+    }
  }
   /*
     TASK 5
